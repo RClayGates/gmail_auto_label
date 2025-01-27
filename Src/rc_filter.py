f2e153f50a5410594344b6b661a1b57784330a09
RClayGates <r.clayton.gates@gmail.com>
2025-01-27 11:55:23 -0500

    added get,list,and delete functions for filter

--- Src/rc_filter.py
+++ Src/rc_filter.py
@@ -3,10 +3,10 @@ import os
 from time import perf_counter
 
 # import: non-std
-from googleapiclient.errors import HttpError
+from googleapiclient.errors import HttpError    # type: ignore
 
 # import: local
-from rc_logger import logger
+from rc_logger import logger    # type: ignore
 
 # const/globals
 log = logger()
@@ -17,8 +17,30 @@ def main():
 
 
 # code blocks
-def filter_create(_service, _label_id, _target_from):
 
+def filter_get(_service,_filter_id):
+    try:
+        result = _service.users().settings().filters().get(userId='me',id=_filter_id).execute()
+        log.debug(f'{result = }')
+
+    except HttpError as _HttpError:
+        # TODO(developer) - Handle errors from gmail API.
+        log.exception(f" {_HttpError = }")
+    except Exception as _E:
+        log.exception(_E.__class__.mro())
+
+def filter_list(_service):
+    try:
+        result = _service.users().settings().filters().list(userId='me').execute()
+        log.debug(f'{result = }')
+    except HttpError as _HttpError:
+        # TODO(developer) - Handle errors from gmail API.
+        log.exception(f" {_HttpError = }")
+    except Exception as _E:
+        log.exception(_E.__class__.mro())
+    return result
+
+def filter_create(_service, _label_id, _target_from):
     filter_obj = {"action": {"addLabelIds": [_label_id]},"criteria": {"from": _target_from}}
     try:
         result = _service.users().settings().filters().create(userId='me',body = filter_obj).execute()
@@ -30,6 +52,17 @@ def filter_create(_service, _label_id, _target_from):
     except Exception as _E:
         log.exception(_E.__class__.mro())
     pass
+
+def filter_delete(_service, _filter_id):
+    try:
+        result = _service.users().settings().filters().delete(userId='me',id=_filter_id).execute()
+        log.debug(f'{result = }')
+
+    except HttpError as _HttpError:
+        # TODO(developer) - Handle errors from gmail API.
+        log.exception(f" {_HttpError = }")
+    except Exception as _E:
+        log.exception(_E.__class__.mro())
 
 
 if __name__ == '__main__':
